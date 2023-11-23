17cab2e45ec73ea7cd02dd516add5975b4acd66b
umarfaruk2 <farukdev22@gmail.com>
2023-11-23 20:07:43 +0600

    uddate food review rating

--- food_review_rating/urls.py
+++ food_review_rating/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('review_rating/<int:pk>/', views.ReviewRatingView.as_view(), name = 'review_rating')    
-    path('get_all_review/<int:pk>/', views.ReviewRatingView.as_view(), name = 'review_rating')    
+    path('review_rating/<int:pk>/', views.ReviewRatingView.as_view(), name = 'review_rating'),
+    path('get_all_review/<int:pk>/', views.AllReviewRatingView.as_view(), name = 'all_review_rating')    
 ]
